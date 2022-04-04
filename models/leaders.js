0e4b365c795149fdc4e8e82b4bd56b4f61115710
Shafin Al Karim <shafin.karim@bongobd.com>
2022-04-05 01:55:27 +0600

    MongoDB integration

---
+++ models/leaders.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+require('mongoose-currency').loadType(mongoose);
+const Schema = mongoose.Schema;
+
+const LeaderSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    designation: {
+        type: String,
+        required: true
+    },
+    abbr: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    featured: {
+        type: Boolean,
+        default: false      
+    }
+}, {
+    timestamps: true
+});
+
+var Leaders = mongoose.model('Leader', LeaderSchema);
+
+module.exports = Leaders;
