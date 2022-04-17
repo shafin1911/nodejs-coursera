13705c579f5e8440db638749637aacf6853cb00f
Shafin Al Karim <shafin.karim@bongobd.com>
2022-04-18 03:16:49 +0600

    passport session based auth

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
 
+const mongoose = require('mongoose');
+var FileStore = require('session-file-store')(session);
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const dishRouter = require('./routes/dishRouter');
 const leaderRouter = require('./routes/leaderRouter');
 const promoRouter = require('./routes/promoRouter');
-
-const mongoose = require('mongoose');
+var passport = require('passport');
+var authenticate = require('./authenticate');
 
 const Dishes = require('./models/dishes');
 
@@ -23,6 +26,7 @@ connect.then((db) => {
 
 var app = express();
 
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -30,11 +34,38 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(cookieParser('12345-67890-09876-54321'));
+
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+function auth (req, res, next) {
+  console.log(req.user);
+
+  if (!req.user) {
+    var err = new Error('You are not authenticated!');
+    err.status = 403;
+    next(err);
+  }
+  else {
+    next();
+  }
+}
+
+app.use(auth);
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/dishes', dishRouter);
 app.use('/leaders', leaderRouter);
 app.use('/promotions', promoRouter);
---
+++ models/user.js
@@ -0,0 +1,30 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var passportLocalMongoose = require('passport-local-mongoose');
+
+var User = new Schema({
+    admin:   {
+        type: Boolean,
+        default: false
+    }
+});
+
+User.plugin(passportLocalMongoose);
+
+// var User = new Schema({
+//     username: {
+//         type: String,
+//         required: true,
+//         unique: true
+//     },
+//     password:  {
+//         type: String,
+//         required: true
+//     },
+//     admin:   {
+//         type: Boolean,
+//         default: false
+//     }
+// });
+
+module.exports = mongoose.model('User', User);
