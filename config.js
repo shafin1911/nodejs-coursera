c49fdc6711a63831c5756c4a8bff893f68235e7a
Shafin Al Karim <shafin.karim@hotmail.com>
2022-04-22 00:23:45 +0600

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     'secretKey': '12345-67890-09876-54321',
     'mongoUrl': 'mongodb://localhost:27017/conFusion',
     'facebook': {
-        clientId: '414508327344558',
-        clientSecret: '4011a712f0c79baab47e4a9b8f280683'
+        clientId: '',
+        clientSecret: ''
     }
 }
